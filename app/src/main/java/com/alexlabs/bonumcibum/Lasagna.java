d26a9b93bbd4baf4a884eb6d03718012e2938416
AlexandrGambarov <aagambarov@gmail.com>
2022-03-22 16:53:16 +0300

    PDF recipe added in activity Lasagna, created new Assets folder 'assets'

--- app/src/main/java/com/alexlabs/bonumcibum/Lasagna.java
+++ app/src/main/java/com/alexlabs/bonumcibum/Lasagna.java
@@ -2,13 +2,24 @@ package com.alexlabs.bonumcibum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 public class Lasagna extends AppCompatActivity {
+
+    PDFView pdf_Lasagna;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lasagna);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        pdf_Lasagna = findViewById(R.id.pdf_Lasagna);
+        pdf_Lasagna.fromAsset("lazagna.pdf").load();
+
     }
 }
